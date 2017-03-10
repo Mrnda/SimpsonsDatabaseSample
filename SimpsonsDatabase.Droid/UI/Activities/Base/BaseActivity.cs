c1e1ffbdf57e446b3cd3eebecb86cffeee451e6a
Michal Mrnustik <michal.mrnustik@outlook.com>
2017-03-10 23:14:37 +0100

    Add base abstract class for Activities

---
+++ SimpsonsDatabase.Droid/UI/Activities/Base/BaseActivity.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.App;
+using SimpsonsDatabase.Data;
+
+namespace SimpsonsDatabase.Droid
+{
+	
+	public abstract class BaseActivity : AppCompatActivity
+	{
+	}
+}
