d38f20c0de57f7604aaf3d2de8bc8c128c23a1d5
Michal Mrnustik <michal.mrnustik@outlook.com>
2017-03-10 23:15:09 +0100

    Add base abstract class for fragment

---
+++ SimpsonsDatabase.Droid/UI/Fragments/Base/BaseFragment.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Com.Lilarcor.Cheeseknife;
+
+namespace SimpsonsDatabase.Droid
+{
+	public abstract class BaseFragment : Fragment
+	{
+	}
+}
