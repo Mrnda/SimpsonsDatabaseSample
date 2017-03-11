efd6529dbb6dca92040357ea29f005580db42dc2
Michal Mrnustik <michal.mrnustik@outlook.com>
2017-03-11 15:33:33 +0100

    Add list viewing

--- SimpsonsDatabase.iOS/MasterViewController.cs
+++ SimpsonsDatabase.iOS/MasterViewController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 
 using Foundation;
+using SimpsonsDatabase.Data.Repositories.Mock;
+using SimpsonsDatabase.Models;
 using UIKit;
 
 namespace SimpsonsDatabase.iOS
@@ -22,20 +24,15 @@ namespace SimpsonsDatabase.iOS
 			Title = "Simpsons Database";
 
 			TableView.Source = dataSource = new DataSource(this);
+			var repository = new MockSimpsonsRepository();
+			dataSource.Objects.AddRange(repository.GetAllSimpsons());
+			TableView.ReloadData();
 		}
 
 		public override void DidReceiveMemoryWarning()
 		{
 			base.DidReceiveMemoryWarning();
 			// Release any cached data, images, etc that aren't in use.
-		}
-
-		void AddNewItem(object sender, EventArgs args)
-		{
-			dataSource.Objects.Insert(0, DateTime.Now);
-
-			using (var indexPath = NSIndexPath.FromRowSection(0, 0))
-				TableView.InsertRows(new[] { indexPath }, UITableViewRowAnimation.Automatic);
 		}
 
 		public override void PrepareForSegue(UIStoryboardSegue segue, NSObject sender)
@@ -52,7 +49,7 @@ namespace SimpsonsDatabase.iOS
 		class DataSource : UITableViewSource
 		{
 			static readonly NSString CellIdentifier = new NSString("Cell");
-			readonly List<object> objects = new List<object>();
+			readonly List<CharacterModel> objects = new List<CharacterModel>();
 			readonly MasterViewController controller;
 
 			public DataSource(MasterViewController controller)
@@ -60,7 +57,7 @@ namespace SimpsonsDatabase.iOS
 				this.controller = controller;
 			}
 
-			public IList<object> Objects
+			public List<CharacterModel> Objects
 			{
 				get { return objects; }
 			}
@@ -81,30 +78,11 @@ namespace SimpsonsDatabase.iOS
 			{
 				var cell = tableView.DequeueReusableCell(CellIdentifier, indexPath);
 
-				cell.TextLabel.Text = objects[indexPath.Row].ToString();
+				cell.TextLabel.Text = objects[indexPath.Row].FullName;
 
 				return cell;
 			}
 
-			public override bool CanEditRow(UITableView tableView, NSIndexPath indexPath)
-			{
-				// Return false if you do not want the specified item to be editable.
-				return true;
-			}
-
-			public override void CommitEditingStyle(UITableView tableView, UITableViewCellEditingStyle editingStyle, NSIndexPath indexPath)
-			{
-				if (editingStyle == UITableViewCellEditingStyle.Delete)
-				{
-					// Delete the row from the data source.
-					objects.RemoveAt(indexPath.Row);
-					controller.TableView.DeleteRows(new[] { indexPath }, UITableViewRowAnimation.Fade);
-				}
-				else if (editingStyle == UITableViewCellEditingStyle.Insert)
-				{
-					// Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-				}
-			}
 		}
 	}
 }
