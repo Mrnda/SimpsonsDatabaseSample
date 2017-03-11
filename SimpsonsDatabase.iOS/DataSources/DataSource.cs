9b26ff842c5fa2287752f36b8483e6d741f5fca9
Michal Mrnustik <michal.mrnustik@outlook.com>
2017-03-11 16:00:20 +0100

    Extract DataSourcer inner to class to its own file

---
+++ SimpsonsDatabase.iOS/DataSources/DataSource.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Foundation;
+using SimpsonsDatabase.Models;
+using UIKit;
+
+namespace SimpsonsDatabase.iOS.DataSources
+{
+	class DataSource : UITableViewSource
+	{
+		static readonly NSString CellIdentifier = new NSString("Cell");
+		readonly List<CharacterModel> objects = new List<CharacterModel>();
+		readonly MasterViewController controller;
+
+		public DataSource(MasterViewController controller)
+		{
+			this.controller = controller;
+		}
+
+		public List<CharacterModel> Objects
+		{
+			get { return objects; }
+		}
+
+		// Customize the number of sections in the table view.
+		public override nint NumberOfSections(UITableView tableView)
+		{
+			return 1;
+		}
+
+		public override nint RowsInSection(UITableView tableview, nint section)
+		{
+			return objects.Count;
+		}
+
+		// Customize the appearance of table view cells.
+		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+		{
+			var cell = tableView.DequeueReusableCell(CellIdentifier, indexPath);
+
+			cell.TextLabel.Text = objects[indexPath.Row].FullName;
+
+			return cell;
+		}
+
+	}
+}
--- SimpsonsDatabase.iOS/ViewControllers/MasterViewController.cs
+++ SimpsonsDatabase.iOS/ViewControllers/MasterViewController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 using Foundation;
 using SimpsonsDatabase.Data.Repositories.Mock;
+using SimpsonsDatabase.iOS.DataSources;
 using SimpsonsDatabase.Models;
 using UIKit;
 
@@ -46,43 +47,6 @@ namespace SimpsonsDatabase.iOS
 			}
 		}
 
-		class DataSource : UITableViewSource
-		{
-			static readonly NSString CellIdentifier = new NSString("Cell");
-			readonly List<CharacterModel> objects = new List<CharacterModel>();
-			readonly MasterViewController controller;
 
-			public DataSource(MasterViewController controller)
-			{
-				this.controller = controller;
-			}
-
-			public List<CharacterModel> Objects
-			{
-				get { return objects; }
-			}
-
-			// Customize the number of sections in the table view.
-			public override nint NumberOfSections(UITableView tableView)
-			{
-				return 1;
-			}
-
-			public override nint RowsInSection(UITableView tableview, nint section)
-			{
-				return objects.Count;
-			}
-
-			// Customize the appearance of table view cells.
-			public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
-			{
-				var cell = tableView.DequeueReusableCell(CellIdentifier, indexPath);
-
-				cell.TextLabel.Text = objects[indexPath.Row].FullName;
-
-				return cell;
-			}
-
-		}
 	}
 }
